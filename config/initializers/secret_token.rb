e4f35f9655a9554f9c5481e60809ca324789e2dc
Amélie Lescure <amelie.lescure@monsieurdrive.com>
2015-02-13 10:23:44 +0100

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PageFetcher::Application.config.secret_key_base = '8dfe55abf1d3a7a937e66f8fee963ad1678edf01ba5a8eafba558a621b476de16e64d04eb5f2bf9a6d600ac794d03a2794d0fc21ab359b4bd9b919cbada63ae3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PageFetcher::Application.config.session_store :cookie_store, key: '_page-fetcher_session'
